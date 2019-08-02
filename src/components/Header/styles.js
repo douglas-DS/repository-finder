71f83aaa791ec45ad51277c9253b48eba28d258c
Douglas Souza <douglas_souza03@hotmail.com>
2019-08-02 01:43:26 -0300

    main app header component and styled file

---
+++ src/components/Header/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 100px;
+    padding: 0 30px;
+    background: #7159f9;
+    color: #FFF;
+    
+    display: flex;
+    align-content: center;
+`;
